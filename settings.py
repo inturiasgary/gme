836a2a03a07e33b4dc4c1c6a294294ec12f9255f
Gary Inturias Rojas <gary@garyDesktop.(none)>
2009-04-16 10:34:18 -0400

    template base adicionado

--- settings.py
+++ settings.py
@@ -47,7 +47,7 @@ MEDIA_URL = ''
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/site_media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'qqxw-529(6@57su-f+#lh63kwif9r(&d0m!paa48yv7-n@18y5'
