de91071fb5752d88a0ad7e6a373582ee9cd81d4f
Gary Inturias Rojas <gary@garyDesktop.(none)>
2009-04-16 19:08:46 -0400

    adicionado urls para cuentas de usuarios y noticias

--- urls.py
+++ urls.py
@@ -1,18 +1,29 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from microblog.views import logout_page
+from django.conf import settings
+from django.views.generic.simple import direct_to_template
+import os 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # XML-RPC
-     (r'^xml_rpc_srv/', 'gme.xmlrpc.rpc_handler'),
+     url(r'^$', direct_to_template, {"template":"homepage.html"}, name="home"),
+     url(r'^xml_rpc_srv/', 'gme.xmlrpc.rpc_handler'),
      url(r'^login/$', 'django.contrib.auth.views.login'),
      url(r'^logout/$',logout_page),
      url(r'^admin/', include(admin.site.urls)),
+     url(r'^account/', include('account.urls')),
+     url(r'^profiles/', include('profiles.urls')),
      
 )
 
+if settings.DEBUG:
+    urlpatterns += patterns('',
+                            (r'^site_media/(?P<path>.*)$','django.views.static.serve',
+                             {'document_root': os.path.join(os.path.dirname(__file__), 'site_media')}),
+                            )
     # Example:
     # (r'^gme/', include('gme.foo.urls')),
 
