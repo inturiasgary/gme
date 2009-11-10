89c639d214e9748042980192d603a2dcf1644a40
Gary Inturias-Laptop <inturiasgary@hotmail.com>
2009-11-10 14:58:03 -0400

    fixed bug in views

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,8 @@
 from django import forms 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from todo.models import Item, List, Comment
 from todo.forms import AddListForm, AddItemForm, EditItemForm
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
 from django.contrib import auth
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
